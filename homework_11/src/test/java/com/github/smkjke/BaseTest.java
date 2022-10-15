ecd7ea8fe9a875d378e7644fc4af0ee4aee4a765
Mikhail Voronov <smirk06@yandex.ru>
2022-10-15 20:03:23 +0300

    homework 11 complete

--- homework_11/src/test/java/com/github/smkjke/BaseTest.java
+++ homework_11/src/test/java/com/github/smkjke/BaseTest.java
@@ -5,10 +5,6 @@ import org.junit.jupiter.api.BeforeEach;
 
 public class BaseTest {
 
-    public static final String SMKJKE_QA_GURU_HOMEWORK = "sMkjke/qa_guru_homework";
-    public static final String ISSUE_NAME = "New Test Issue";
-
-
     @BeforeEach
     void setUp() {
         Configuration.baseUrl = "https://github.com";
